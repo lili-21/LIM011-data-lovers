ca6dfc11de545b237753c26ebb3a32982e7921d3
lili-21 <liliana.nolasco@gmail.com>
2019-10-29 13:34:46 -0500

    avance con la parte del inico de html

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
+
+const btnEnter = document.getElementById('enter');
+btnEnter.addEventListener('click', () => {
+    document.getElementById('root').classList.remove('hide');
+    document.getElementById('home').classList.add('hide');
+});
+import POTTER from './data/potter/potter.js'
+
 /**
  * import POKEMON from './data/pokemon/pokemon.js'
  * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
+ * '
  */
 
-import { example } from './data.js';
+/*import { example } from './data.js';
 
-console.log(example);
+
+console.log(example);*/
 
 /*
- * console.log(POKEMON);
- * console.log(LoL);
+ 
  * console.log(POTTER)
 */
