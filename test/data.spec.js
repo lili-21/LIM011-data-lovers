cfefdab0c993bb1319c1c721e58b46801cee1085
lili-21 <liliana.nolasco@gmail.com>
2019-11-15 11:12:34 -0500

    test de las casas, varitas, patronus y ordenar alfabeticamente

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,5 @@
 // importamos la función `example`
-import { sortData } from '../src/data.js';
+import { sortData, filterData } from '../src/data.js';
 import POTTER from '../src/data/potter/potter.js';
 
 describe('sortData', () => {
@@ -9,5 +9,47 @@ describe('sortData', () => {
 
   it('debería ... ascendente', () => {
     expect(sortData(POTTER, 'ascendente')[0].name).toBe('Argus Filch');
+  });
+  it('debería ... descendente', () => {
+    expect(sortData(POTTER, 'descendente')[0].name).toBe('Vincent Crabbe');
+  });
+});
+
+describe('filterData', () => {
+  it('debería ser una función', () => {
+    expect(typeof filterData).toBe('function');
+  });
+  it('debería traer todos los elementos', () => {
+    expect(filterData(POTTER, null)).toHaveLength(POTTER.length);
+  });
+  it('debería ... Gryffindor', () => {
+    expect(filterData(POTTER, { field: 'house', value: 'Gryffindor' })[0].house).toBe('Gryffindor');
+  });
+  it('debería ... Hufflepuff', () => {
+    expect(filterData(POTTER, { field: 'house', value: 'Hufflepuff' })[0].house).toBe('Hufflepuff');
+  });
+  it('debería ... Slytherin', () => {
+    expect(filterData(POTTER, { field: 'house', value: 'Slytherin' })[0].house).toBe('Slytherin');
+  });
+  it('debería ... Ravenclaw', () => {
+    expect(filterData(POTTER, { field: 'house', value: 'Ravenclaw' })[0].house).toBe('Ravenclaw');
+  });
+  it('debería ... dragon', () => {
+    expect(filterData(POTTER, { field: ['wand', 'core'], value: 'dragon heartstring' })[0].wand.core).toBe('dragon heartstring');
+  });
+  it('debería ... fenix', () => {
+    expect(filterData(POTTER, { field: ['wand', 'core'], value: 'phoenix feather' })[0].wand.core).toBe('phoenix feather');
+  });
+  it('debería ... Unicornio', () => {
+    expect(filterData(POTTER, { field: ['wand', 'core'], value: ('unicorn tail-hair', 'unicorn hair') })[0].wand.core).toBe(('unicorn tail-hair', 'unicorn hair'));
+  });
+  it('debería ... salvajes', () => {
+    const listaSalvajes = ['stag', 'otter', 'swan', 'doe', 'hare', 'hare', 'wolf', 'weasel', 'cedar', 'lynx'];
+    expect(listaSalvajes.includes(filterData(POTTER, { field: 'patronus', value: listaSalvajes })[0].patronus)).toBe(true);
+  });
+  it('debería ... domesticos', () => {
+    const listaPatronus = ['Jack Russell terrier', 'tabby cat', 'persian cat', 'horse'];
+
+    expect(listaPatronus.includes(filterData(POTTER, { field: 'patronus', value: listaPatronus })[0].patronus)).toBe(true);
   });
 });
